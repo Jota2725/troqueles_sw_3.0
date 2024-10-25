0fbc2cee4ac46d9660d05b04e418029e0bedcab6
Runyy <146892210+Rodaverme@users.noreply.github.com>
2024-10-25 14:26:25 -0500

    Barra de busqueda

--- lib/main.dart
+++ lib/main.dart
@@ -55,41 +55,55 @@ final List<Troquel> _listTroquel = [
 ];
 
 Widget _buildTable({bool sortAsscending = true}) {
-  return Container(
-    height: double.infinity,
-  
-    child: DataTable(
-      sortColumnIndex: 0,
-      sortAscending: sortAsscending,
-      columns: const <DataColumn>[
-        DataColumn(label: Row(
-          
-          
-          children: [
-            Icon(
-                Icons.location_on_rounded,
-                size: 15,
-              ),
-              SizedBox(width: 15,),
-            Text('Ubicación'),
-          ],
-        )),
-        DataColumn(label: Text('Gico')),
-        DataColumn(label: Text('Cliente')),
-        DataColumn(label: Text('Referencia')),
-        DataColumn(label: Text('Maquina')),
+  return SingleChildScrollView(
+    scrollDirection: Axis.vertical,
+    child: Column(
+      children: [
+        const SizedBox(
+          height: 60,
+          width: double.infinity,
+          child:
+              Padding(padding: EdgeInsets.all(6.0), child: CustomSearchBar()),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: DataTable(
+            sortColumnIndex: 0,
+            sortAscending: sortAsscending,
+            columns: const <DataColumn>[
+              DataColumn(
+                  label: Row(
+                children: [
+                  Icon(
+                    Icons.location_on_rounded,
+                    size: 15,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text('Ubicación'),
+                ],
+              )),
+              DataColumn(label: Text('Gico')),
+              DataColumn(label: Text('Cliente')),
+              DataColumn(label: Text('Referencia')),
+              DataColumn(label: Text('Maquina')),
+            ],
+            rows: _listTroquel.map<DataRow>((Troquel troquel) {
+              return DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('${troquel.ubicacion}')),
+                  DataCell(Text('${troquel.gico}'),
+                      showEditIcon: true, onTap: () {}),
+                  DataCell(Text(troquel.cliente)),
+                  DataCell(Text('${troquel.referencia}')),
+                  DataCell(Text(troquel.maquina)),
+                ],
+              );
+            }).toList(),
+          ),
+        ),
       ],
-      rows: _listTroquel.map<DataRow>((Troquel troquel) {
-        return DataRow(
-          cells: <DataCell>[
-            DataCell( Text('${troquel.ubicacion}')),    
-            DataCell(Text('${troquel.gico}'), showEditIcon: true, onTap: () {}),
-            DataCell(Text(troquel.cliente)),
-            DataCell(Text('${troquel.referencia}')),
-            DataCell(Text(troquel.maquina)),
-          ],
-        );
-      }).toList(),
     ),
   );
 }
@@ -101,82 +115,129 @@ class MyhomeWidget extends StatefulWidget {
   State<MyhomeWidget> createState() => _MyhomeWidgetState();
 }
 
-
-
 class _MyhomeWidgetState extends State<MyhomeWidget> {
- 
+  final ScrollController controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-   final size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('TROQUELES SMURFIT WESTROCK'),
-      ),
+          title: Text('TROQUELES SMURFIT WESTROCK',
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Color.fromRGBO(23, 13, 171, 1),
+          centerTitle: true),
       body: Row(
         children: [
-          Container(
-           
-            height: size.height,
-            width: size.width*0.2,
-            child: Column(children: [
-              ListTile(
-                leading: Icon(Icons.book_rounded) ,
-                trailing: Icon(Icons.arrow_forward_ios_outlined),
-
-                  title: Text('Bibliaco Troqueles'),
-                  onTap: (){}
-
-              ),
-              ListTile(
-                leading: Icon(Icons.autorenew) ,
-                trailing: Icon(Icons.arrow_forward_ios_outlined),
-
-                  title: Text('Troqueles en proceso'),
-                  onTap: (){}
-
-              ),
-              ListTile(
-                leading: Icon(Icons.book_rounded) ,
-                trailing: Icon(Icons.arrow_circle_right_outlined),
-
-                  title: Text('Bibliaco Troqueles'),
-                  onTap: (){}
-
-              ),
-              ListTile(
-                leading: Icon(Icons.book_rounded) ,
-                trailing: Icon(Icons.arrow_circle_right_outlined),
-
-                  title: Text('Bibliaco Troqueles'),
-                  onTap: (){}
-
-              ),
-              
-              
-              ],
-
-            ),
-            
-
-
+          menuTroqueles(size: size),
+          SizedBox(
+            width: size.width * 0.8,
+            // Ancho completo de la pantalla
+            child: _buildTable(),
           ),
-           SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: SizedBox(
-                    width: size.width*0.8
-
-                        
-                      , // Ancho completo de la pantalla
-                    child: _buildTable(),
-                  )),
         ],
       ),
     );
   }
 }
 
+class menuTroqueles extends StatelessWidget {
+  const menuTroqueles({
+    super.key,
+    required this.size,
+  });
 
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height,
+      width: size.width * 0.2,
+      child: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            child: Image(
+                image: AssetImage('assets/Smurfit_Westrock_(logo).png')),
+          ),
+          ListTile(
+               hoverColor: Colors.blue.shade100,
+              leading: const Icon(Icons.book_rounded),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              title: const Text('Bibliaco Troqueles'),
+              onTap: () {}),
+          const SizedBox(
+            height: 10,
+          ),
+          ListTile(
+               hoverColor: Colors.blue.shade100,
+              leading: const Icon(Icons.autorenew),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              title: const Text('Troqueles en proceso'),
+              onTap: () {}),
+          const SizedBox(
+            height: 10,
+          ),
+          ListTile(
+              hoverColor: Colors.blue.shade100,
+              leading: const Icon(Icons.fact_check_outlined),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              title: const Text('Troqueles Terminados'),
+              onTap: () {}),
+          const SizedBox(
+            height: 10,
+          ),
+          ListTile(
+              hoverColor: Colors.blue.shade100,
+              leading: const Icon(Icons.construction),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              title: const Text('Troqueles en mantenimiento'),
+              onTap: () {}),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchAnchor(
+      builder: (BuildContext context, SearchController controller) {
+        return SearchBar(
+          elevation: const WidgetStatePropertyAll(0),
+          controller: controller,
+          padding: const WidgetStatePropertyAll<EdgeInsets>(
+              EdgeInsets.symmetric(horizontal: 16.0)),
+          onTap: () {
+            controller.openView();
+          },
+          onChanged: (_) {
+            controller.openView();
+          },
+          leading: const Icon(Icons.search),
+        );
+      },
+      suggestionsBuilder: (BuildContext context, SearchController controller) {
+        return List<ListTile>.generate(5, (int index) {
+          final String item = 'item $index';
+          return ListTile(
+            title: Text(item),
+            onTap: () {
+              controller.closeView(item);
+            },
+          );
+        });
+      },
+    );
+  }
+}
 
 class Troquel {
   final int ubicacion;
